26f0aa24f2847d5f26b36881f20473c57da0b3d3
Kevin Cheung <44646134+kcheung3102@users.noreply.github.com>
2020-01-28 12:42:26 -0500

    Update MinMaxSum.js

--- MinMaxSum/MinMaxSum.js
+++ MinMaxSum/MinMaxSum.js
@@ -6,6 +6,7 @@ function miniMaxSum(arr) {
     const maxSet = data.slice(1); //removes first data in array
     const minSet = data.slice(0, -1); //removes last data in array
     const reducer = (a, total ) => a + total;
+    
     const maxNum = maxSet.reduce(reducer, 0); //adds up all the integers in maxSet array
     const minNum = minSet.reduce(reducer, 0); //adds up all the integers in minSet array
 
