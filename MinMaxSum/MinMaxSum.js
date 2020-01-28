06f29bb0e5a520a2528de4a09a1bbc7fa27d0fbb
Kevin Cheung <44646134+kcheung3102@users.noreply.github.com>
2020-01-28 12:41:46 -0500

    Create MinMaxSum.js

---
+++ MinMaxSum/MinMaxSum.js
@@ -0,0 +1,13 @@
+function miniMaxSum(arr) {
+
+    //sort array
+    const data = arr.sort();
+
+    const maxSet = data.slice(1); //removes first data in array
+    const minSet = data.slice(0, -1); //removes last data in array
+    const reducer = (a, total ) => a + total;
+    const maxNum = maxSet.reduce(reducer, 0); //adds up all the integers in maxSet array
+    const minNum = minSet.reduce(reducer, 0); //adds up all the integers in minSet array
+
+    console.log(minNum, maxNum);
+    
